c75470346da9aad62e988da823cb147eb7e0320a
marti <marti@DESKTOP-D4C92QL>
2019-01-17 11:35:27 -0800

    solution created

---
+++ desktopapplication/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktopapplication
+{
+    class Class1
+    {
+        string a;
+
+        public Class1(string a)
+        {
+            this.a = a;
+        }
+    }
+}
