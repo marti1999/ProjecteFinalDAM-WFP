f996c73597d75dbde750e583ad1f359d3a7e7f25
marti <marti@DESKTOP-D4C92QL>
2019-01-21 12:10:12 -0800

    Creació de l´estructura del projecte

---
+++ desktopapplication/ViewModel/Vista1ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktopapplication.ViewModel
+{
+    class Vista1ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Vista1ViewModel()
+        {
+            //TODO inits y tal
+        }
+    }
+}
