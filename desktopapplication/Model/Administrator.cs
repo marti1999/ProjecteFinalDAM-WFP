1cf59e3f594fe3739849f4aacfc3de91dc8c5720
marti <marti@DESKTOP-65875CT>
2019-01-29 18:25:48 +0100

    repository

--- desktopapplication/Model/Administrator.cs
+++ desktopapplication/Model/Administrator.cs
@@ -19,7 +19,7 @@ namespace desktopapplication.Model
         public System.DateTime dateCreated { get; set; }
         public bool isSuper { get; set; }
         public bool active { get; set; }
-        public string codiEmpleat { get; set; }
+        public string workerCode { get; set; }
         public int Language_Id { get; set; }
         public int Warehouse_Id { get; set; }
 
--- desktopapplication/Model/Repository.cs
+++ desktopapplication/Model/Repository.cs
@@ -1,14 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows;
 
 namespace desktopapplication.Model
 {
-    class Repository
+    public class Repository
     {
+        private static string ws1 = "http://localhost:12345/api/";
+        public static List<Administrator> getAllAdministrators()
+        {
+            List<Administrator> lc = new List<Administrator>();
+           // lc = (List<Administrator>)MakeRequest(string.Concat(ws1, "administratorsTot/"), null, "GET", "application/json", typeof(List<Administrator>));
+            return lc;
+        }
 
-        private static string ws = "http://172.16.12.1:51057/api/";
+       
+
+        public static object MakeRequest(string requestUrl, object JSONRequest, string JSONmethod, string JSONContentType, Type JSONResponseType)
+        //  requestUrl: Url completa del Web Service, amb l'opció sol·licitada
+        //  JSONrequest: objecte que se li passa en el body (només per a POST/PUT)
+        //  JSONmethod: "GET"/"POST"/"PUT"/"DELETE"
+        //  JSONContentType: "application/json" en els casos que el Web Service torni objectes
+        //  JSONRensponseType:  tipus d'objecte que torna el Web Service (typeof(tipus))
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest; //WebRequest WR = WebRequest.Create(requestUrl);   
+                string sb = JsonConvert.SerializeObject(JSONRequest);
+                request.Method = JSONmethod;  // "GET"/"POST"/"PUT"/"DELETE";  
+
+                if (JSONmethod != "GET")
+                {
+                    request.ContentType = JSONContentType; // "application/json";   
+                    Byte[] bt = Encoding.UTF8.GetBytes(sb);
+                    Stream st = request.GetRequestStream();
+                    st.Write(bt, 0, bt.Length);
+                    st.Close();
+                }
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.StatusDescription));
+
+                    Stream stream1 = response.GetResponseStream();
+                    StreamReader sr = new StreamReader(stream1);
+                    string strsb = sr.ReadToEnd();
+                    object objResponse = null;
+
+                    //objResponse = JsonConvert.DeserializeObject(strsb, JSONResponseType);
+                    return objResponse;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
     }
 }
