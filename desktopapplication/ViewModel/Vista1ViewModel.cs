32334985a7a318481963d37af86e2aaac1d77f5c
VAuyanet <victor.auyanet@gmail.com>
2019-02-19 20:32:23 +0100

    Canvis menu lateral

--- desktopapplication/ViewModel/Vista1ViewModel.cs
+++ desktopapplication/ViewModel/Vista1ViewModel.cs
@@ -24,6 +24,8 @@ namespace desktopapplication.ViewModel
 
         public ICommand homeClickCommand { get; set; }
         public ICommand usersClickCommand { get; set; }
+        public ICommand clothesClickCommand { get; set; }
+        public ICommand announcementsClickCommand { get; set; }
         public ICommand createAnnouncementCommand { get; set; }
 
 
@@ -39,6 +41,8 @@ namespace desktopapplication.ViewModel
         {
             homeClickCommand = new RelayCommand(x => selectHome());
             usersClickCommand = new RelayCommand(x => selectUsers());
+            clothesClickCommand = new RelayCommand(x => selectClothes());
+            announcementsClickCommand = new RelayCommand(x => selectAnnouncements());
             createAnnouncementCommand = new RelayCommand(x => createAnnouncement());
 
             populateUsers();
@@ -290,8 +294,6 @@ namespace desktopapplication.ViewModel
 
         private void selectHome()
         {
-            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ca");
-            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ca");
             SelectedTab = 1;
             //HomeSelected = Visibility.Visible;
             //  UsersSelected = Visibility.Hidden;
@@ -304,6 +306,22 @@ namespace desktopapplication.ViewModel
             // UsersSelected = Visibility.Visible;
             // HomeSelected = Visibility.Hidden;
             Console.WriteLine("users selected");
+        }
+
+        private void selectClothes()
+        {
+            SelectedTab = 2;
+            //HomeSelected = Visibility.Visible;
+            //  UsersSelected = Visibility.Hidden;
+            Console.WriteLine("CLOTHES SELECTED");
+        }
+
+        private void selectAnnouncements()
+        {
+            SelectedTab = 3;
+            //HomeSelected = Visibility.Visible;
+            //  UsersSelected = Visibility.Hidden;
+            Console.WriteLine("ANNOUNCEMENTS SELECTED");
         }
 
 
