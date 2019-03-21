4423ab33c202f82e89c11bfe887df7d36f2c1202
marti <marti@DESKTOP-65875CT>
2019-03-21 19:49:17 +0100

    alguns canvis a la interficie. El login envia la meitat del SHA512 de la password

--- desktopapplication/ViewModel/loginViewModel.cs
+++ desktopapplication/ViewModel/loginViewModel.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -71,8 +73,26 @@ namespace desktopapplication.ViewModel
 
             Administrator a = new Administrator();
             a.email = AdminUsername;
+            string passwordRaw = passwordVar.Password;
+
+            var data = Encoding.UTF8.GetBytes(passwordRaw);
+
+
+            byte[] hash;
+
+            using (SHA512 sha = new SHA512Managed())
+            {
+                hash = sha.ComputeHash(data);
+            }
+
+            string hashString = System.Text.Encoding.Default.GetString(hash);
+            Console.WriteLine(hashString);
+
+            hashString = hashString.Substring(0, 32);
+
+
             AdminPassword = passwordVar.Password;
-            a.password = AdminPassword;
+            a.password = hashString;
 
             if (Repository.loginAdministrator(a))
             {
@@ -90,6 +110,50 @@ namespace desktopapplication.ViewModel
                 Console.WriteLine("Incorrect email/password");
             }
             //     System.Windows.Forms.Application.Exit();
+        }
+
+
+
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+
         }
 
 
