9656a16549f9eaf5305c9932323b42dfa18982f1
marti <marti@DESKTOP-65875CT>
2019-03-22 19:12:12 +0100

    es pot esborrar una roba i a la pantalla de login s' ha millorat el disseny (fa resize, menys o menys, i hi ha les icones davant dels inputs

--- desktopapplication/Model/ClothesRepository.cs
+++ desktopapplication/Model/ClothesRepository.cs
@@ -32,6 +32,11 @@ namespace desktopapplication.Model.objects
             return c2;
         }
 
+        public static void deleteCloth(Cloth c)
+        {
+        Cloth c2 = (Cloth) MakeRequest(string.Concat(ws1, "cloth/", c.Id), null, "DELETE", "application/json", typeof(Cloth));
+        }
+
 
         public static object MakeRequest(string requestUrl, object JSONRequest, string JSONmethod, string JSONContentType, Type JSONResponseType)
         //  requestUrl: Url completa del Web Service, amb l'opció sol·licitada
